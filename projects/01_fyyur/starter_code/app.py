614ae3e5d85caa61544d519c37c7f11a7ee81880
carlos valarezo <carlos.valarezo@gmail.com>
2021-06-23 04:52:21 -0500

    @carlosvalarezo search artists from the db

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -318,13 +318,21 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
+    # response = {
+    #     "count": 1,
+    #     "data": [{
+    #         "id": 4,
+    #         "name": "Guns N Petals",
+    #         "num_upcoming_shows": 0,
+    #     }]
+    # }
+    search_term = request.form.get('search_term', '')
+    search = f"%{search_term}%"
+    count = Artist.query.filter(Artist.name.like(search)).count()
+    data = Artist.query.filter(Artist.name.like(search)).all()
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": count,
+        "data": data
     }
     return render_template('pages/search_artists.html', results=response,
                            search_term=request.form.get('search_term', ''))
