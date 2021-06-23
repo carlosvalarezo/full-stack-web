087cf3b1cff2b503002f3cdd8ed95b20aa6f0d1d
carlos valarezo <carlos.valarezo@gmail.com>
2021-06-22 19:24:44 -0500

    @carlosvalarezo solves docker-compose issues & links the code with the db

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +8,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+DB_NAME = os.getenv("DB_NAME", "fyyur")
+DB_USER = os.getenv("DB_USER", "fyyur")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "fyyur")
+DB_PORT = os.getenv("DB_PORT", 5432)
+DB_HOST = os.getenv("DB_HOST", "db")
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
