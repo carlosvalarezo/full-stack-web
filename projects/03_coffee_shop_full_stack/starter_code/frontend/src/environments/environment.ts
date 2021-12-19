eb2b3fa058e50132b3cdcfe8352a83dfa848d664
carlos valarezo <carlos.valarezo@gmail.com>
2021-12-19 06:56:06 -0500

    Updates dockerfile &  docker-compose to run the angular/ionic application
    - Serves the ng server to be accessible from out of docker

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     url: '', // the auth0 domain prefix
     audience: '', // the audience set for the auth0 app
     clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
   }
 };
