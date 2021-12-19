c9b8cffa07ec8a907c624f0582e1de2fc2fcd3c6
carlos valarezo <carlos.valarezo@gmail.com>
2021-12-19 07:10:36 -0500

    Adds support fot https in angular since Auth0 does not recognizes http, only https

--- projects/03_coffee_shop_full_stack/starter_code/backend/start.sh
+++ projects/03_coffee_shop_full_stack/starter_code/backend/start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 mkdir -p $PWD/backend/src/certs
+mkdir -p $PWD/frontend/certs
 openssl req -x509 -out $PWD/backend/src/certs/localhost.crt \
         -keyout $PWD/backend/src/certs/localhost.key \
         -newkey rsa:2048 \
         -nodes -sha256 \
         -subj '/CN=localhost' -extensions EXT \
         -config $PWD/backend/src/cert_config.cf
+cp $PWD/backend/src/certs/* $PWD/frontend/certs/
 docker build . -t carlosvalarezo/coffee_shop_backend
 #docker run -itp 10443:443 --rm --name coffee_shop_backend \
 #       -v $PWD:/coffee_shop_backend \
